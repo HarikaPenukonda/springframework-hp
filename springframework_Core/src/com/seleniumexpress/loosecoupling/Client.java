ecf7b18c583ca323ab583778253ef031b59a2914
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-08 17:24:41 -0600

    basic project for loose coupling

---
+++ springframework_Core/src/com/seleniumexpress/loosecoupling/Client.java
@@ -0,0 +1,30 @@
+package com.seleniumexpress.loosecoupling;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans4.xml");
+		System.out.println("config file loaded...");
+		Student stu = context.getBean("student",Student.class);
+		stu.cheat();
+
+		
+		/* Here, if we add another student who is using the same mathCheat 
+		 * reference, our current code leads to create mathCheat object two 
+		 * times, so if we create 100 students it creates 100 mathCheat object
+		 * which is a very bad coding practice.
+		 * */		
+		
+		/*
+		 * Student student = new Student(); 
+		 * MathCheat cheat = new MathCheat();
+		 * student.setMathCheat(cheat);
+		 */
+		
+	}
+
+}
---
+++ springframework_Core/src/com/seleniumexpress/loosecoupling/MathCheat.java
@@ -0,0 +1,13 @@
+package com.seleniumexpress.loosecoupling;
+
+public class MathCheat {
+	
+	public MathCheat(){
+		System.out.println("MathCheat constructor is called...");
+	}
+	
+	public void cheating() {
+		System.out.println("Math cheating started...");
+	}
+
+}
