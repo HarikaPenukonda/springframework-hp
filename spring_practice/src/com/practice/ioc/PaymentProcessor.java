c935747c321a3049b59d313bb8bd12aacf920a0d
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-07 18:18:47 -0600

    using interface reference instead of class

--- spring_practice/src/com/practice/ioc/PaymentProcessor.java
+++ spring_practice/src/com/practice/ioc/PaymentProcessor.java
@@ -8,9 +8,11 @@ public class PaymentProcessor {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		System.out.println("loading.. config file..");
+		PaymentService bean = context.getBean("payment",PaymentService.class);
+		bean.processPayment();
 		//CreditCardPaymentService credit = context.getBean("cc",CreditCardPaymentService.class);
-		PayPalPaymentService paypal = context.getBean("pp",PayPalPaymentService.class);
-		paypal.processPayment();
+		//PayPalPaymentService paypal = context.getBean("pp",PayPalPaymentService.class);
+		//paypal.processPayment();
 	}
 
 }
