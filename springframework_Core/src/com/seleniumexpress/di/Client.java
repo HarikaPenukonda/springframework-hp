ca8199ab19fb246de276ebf2d664aeabe9b1f535
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-08 12:08:10 -0600

    bad coding practice

--- springframework_Core/src/com/seleniumexpress/di/Client.java
+++ springframework_Core/src/com/seleniumexpress/di/Client.java
@@ -14,6 +14,13 @@ public class Client {
 		
 		AnotherStudent ans = context.getBean("anotherstudent",AnotherStudent.class);
 		ans.cheat();
+		
+		/* Here, if we add another student who is using the same mathCheat 
+		 * reference, our current code leads to create mathCheat object two 
+		 * times, so if we create 100 students it creates 100 mathCheat object
+		 * which is a very bad coding practice.
+		 * */		
+		
 		/*
 		 * Student student = new Student(); 
 		 * MathCheat cheat = new MathCheat();
