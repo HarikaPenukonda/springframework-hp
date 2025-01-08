60869ee03fcd90b4d2a16fcaf4bc15a5ad6e2d95
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-08 12:13:46 -0600

    code refractor

--- springframework_Core/src/com/seleniumexpress/di/MathCheat.java
+++ springframework_Core/src/com/seleniumexpress/di/MathCheat.java
@@ -2,6 +2,10 @@ package com.seleniumexpress.di;
 
 public class MathCheat {
 	
+	public MathCheat(){
+		System.out.println("MathCheat constructor is called...");
+	}
+	
 	public void cheating() {
 		System.out.println("Math cheating started...");
 	}
