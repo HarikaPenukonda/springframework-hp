81552f432ddd3e570a0bfb886579443590e6d8dc
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-09 12:45:25 -0600

    bean creation in xml

---
+++ spring_core_annotations/src/spring_core_annotations/Client.java
@@ -0,0 +1,16 @@
+package spring_core_annotations;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		System.out.println("config file... loaded...");
+		College bean = context.getBean("college",College.class);
+		System.out.println("The bean is created by spring " + bean);
+	}
+
+}
---
+++ spring_core_annotations/src/spring_core_annotations/College.java
@@ -0,0 +1,5 @@
+package spring_core_annotations;
+
+public class College {
+
+}
