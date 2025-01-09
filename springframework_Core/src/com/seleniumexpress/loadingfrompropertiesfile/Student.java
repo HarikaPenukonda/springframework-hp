bffc686e37e4e2f801cbf0945768444baa3358ea
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-08 22:01:38 -0600

    injecting static values using value annotation

--- springframework_Core/src/com/seleniumexpress/loadingfrompropertiesfile/Student.java
+++ springframework_Core/src/com/seleniumexpress/loadingfrompropertiesfile/Student.java
@@ -1,17 +1,24 @@
 package com.seleniumexpress.loadingfrompropertiesfile;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Student {
 	
 	private String name;
 	private String interestedCourse;
 	private String hobby;
 	
+	@Value("Admin")
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	@Value("Python")
 	public void setInterestedCourse(String interestedCourse) {
 		this.interestedCourse = interestedCourse;
 	}
+	
+	@Value("Coding")
 	public void setHobby(String hobby) {
 		this.hobby = hobby;
 	}
