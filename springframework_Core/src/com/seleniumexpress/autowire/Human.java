35e3078b2d3d2633c17b3a51c9d7d7331d1eb1c4
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-08 19:32:54 -0600

    autowired before dependency

--- springframework_Core/src/com/seleniumexpress/autowire/Human.java
+++ springframework_Core/src/com/seleniumexpress/autowire/Human.java
@@ -5,23 +5,24 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Human {
 	
+	@Autowired // automatic wiring of dependencies
+	@Qualifier("octopusHeart") // letting spring know which particular object it needs to perform dependency injection
 	private Heart heart;
 	
-	public Human() {
-		
-	}
-	
-	public Human(Heart heart) {
-		super();
-		this.heart = heart;
-	}
-	
-	@Autowired
-	@Qualifier("octopusHeart")
-	public void setHeart(Heart heart) {
-		this.heart = heart;
-		System.out.println("setter method called...");
-	}
+//	public Human() {
+//		
+//	}
+//	
+//	public Human(Heart heart) {
+//		super();
+//		this.heart = heart;
+//	}
+//	
+//	
+//	public void setHeart(Heart heart) {
+//		this.heart = heart;
+//		System.out.println("setter method called...");
+//	}
 	
 	public void pump() {
 		if(heart != null) {
