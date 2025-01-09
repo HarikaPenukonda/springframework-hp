a5e6543cc75a10c414e6da8cbf21fa9a30666c2e
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-08 19:38:11 -0600

    notes

--- springframework_Core/src/com/seleniumexpress/autowire/Human.java
+++ springframework_Core/src/com/seleniumexpress/autowire/Human.java
@@ -8,22 +8,11 @@ public class Human {
 	@Autowired // automatic wiring of dependencies
 	@Qualifier("octopusHeart") // letting spring know which particular object it needs to perform dependency injection
 	private Heart heart;
-	
-//	public Human() {
-//		
-//	}
-//	
-//	public Human(Heart heart) {
-//		super();
-//		this.heart = heart;
-//	}
-//	
-//	
-//	public void setHeart(Heart heart) {
-//		this.heart = heart;
-//		System.out.println("setter method called...");
-//	}
-	
+
+	/*	If match found for `octopusHeart` in beans.xml then directly
+	 *  creates `octopusHeart` object and inject that to the dependency called heart
+	 *  first it checks byType and then byName
+	 * */
 	public void pump() {
 		if(heart != null) {
 			heart.startPumping();
