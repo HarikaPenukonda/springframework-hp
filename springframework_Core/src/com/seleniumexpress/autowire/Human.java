96ac5aace038ae556d9f180a3759acc6ed5a4667
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-08 18:51:44 -0600

    autowiring - constructor

--- springframework_Core/src/com/seleniumexpress/autowire/Human.java
+++ springframework_Core/src/com/seleniumexpress/autowire/Human.java
@@ -3,6 +3,11 @@ package com.seleniumexpress.autowire;
 public class Human {
 	
 	private Heart heart;
+	
+	public Human(Heart heart) {
+		super();
+		this.heart = heart;
+	}
 
 	public void setHeart(Heart heart) {
 		this.heart = heart;
