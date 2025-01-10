55f786fa68137615e28a19a3d8b92446fce9c272
HarikaPenukonda <penukondaharika@gmail.com>
2025-01-09 19:31:24 -0600

    getting all rows from DB - JDBC

---
+++ spring_bean_lifecycle/src/com/seleniumexpress/beanlifecycle/StudentDAO.java
@@ -0,0 +1,41 @@
+package com.seleniumexpress.beanlifecycle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StudentDAO {
+	
+	private String driver = "com.mysql.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/Food?useSSL=false"; 
+	private String userName = "root";
+	private String password = "admin";
+	
+	public void selectAllRows() throws ClassNotFoundException, SQLException {
+		
+		// load the driver
+		Class.forName(driver);
+		
+		// get a connection
+		Connection con = DriverManager.getConnection(url, userName, password);
+		
+		// create a statement
+		Statement stmt = con.createStatement();
+		
+		// execute the query
+		ResultSet rs = stmt.executeQuery("SELECT * FROM Food.HostelStudentInfo;");
+		
+		while(rs.next()) {
+			int student_id = rs.getInt(1);
+			String student_name = rs.getString(2);
+			String hostel_fee = rs.getString(3);
+			String food_type = rs.getString(4);
+			
+			System.out.println(student_id + " " + student_name + " " + hostel_fee + " " + food_type);
+		}
+	}
+	
+
+}
---
+++ spring_bean_lifecycle/src/com/seleniumexpress/beanlifecycle/Test.java
@@ -0,0 +1,14 @@
+package com.seleniumexpress.beanlifecycle;
+
+import java.sql.SQLException;
+
+public class Test {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		StudentDAO dao = new StudentDAO();
+		dao.selectAllRows();
+
+	}
+
+}
